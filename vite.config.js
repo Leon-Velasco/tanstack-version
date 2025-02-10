7e19b91cd5fd694025eebd7442656552ceaa3dfb
Leon Velasco <lvelasco@intelitax.com>
2025-02-10 13:53:44 -0600

    sricpt update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://leon-brand.github.io/tanstack-table'
+  base: 'https://Leon-Velasco.github.io/tanstack-version'
 })
